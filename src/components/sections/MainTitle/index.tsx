b03b6ce20500d9847e2926a820da40897e69a3b9
Jynn Park <yuunnii315@gmail.com>
2020-10-18 13:42:31 +0900

    Change welcome message by time

--- src/components/sections/MainTitle/index.tsx
+++ src/components/sections/MainTitle/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { User } from '../../../utils';
 import './index.css';
 
@@ -7,9 +7,28 @@ type MainTitleProps = {
 }
 
 const MainTitle = ({user}: MainTitleProps) => {
+    const [welcomeMessage, setWelcomeMessage] = useState<string>();
+
+    useEffect(() => {
+        let time= new Date().getHours();
+        switch(true){
+            case (5 <= time && time < 12):
+               setWelcomeMessage("Good morning");
+               break;
+            
+            case (12 <= time && time < 18):
+                setWelcomeMessage("Good afternoon");
+                break;
+            
+            case (18 <= time || time < 5):
+                setWelcomeMessage("Good evening");
+               break;
+        }
+    },[])
+
     return(
         <div>
-            <div className="main-title">Good morning, {user.name}</div>
+            <div className="main-title">{welcomeMessage}, {user.name}</div>
             <div className="main-subtitle">Quick touch &amp; express your love</div>
         </div>
     )
