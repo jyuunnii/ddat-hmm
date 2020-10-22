e26d127ef5fc507da8e14dc7ab348a5cc21753b7
Jynn Park <yuunnii315@gmail.com>
2020-10-22 10:35:09 +0900

    Change header logo

--- src/components/common/Logo/index.tsx
+++ src/components/common/Logo/index.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import './index.css';
 
-const Logo = () => {
-return(
-    <div className="logo">DDAT HMM</div>
-)
+type LogoProps = {
+   pathname: string;
+}
+
+const Logo = ({pathname}: LogoProps) => {
+    let logo = "DDAT HMM";
+    switch(pathname){
+        case "/":
+            logo = "DDAT HMM"
+            break;
+
+        case "/signin":
+            logo = "DDAT HMM"
+            break;
+
+        case "/signup":
+            logo = "DDAT HMM"
+            break;
+
+        case "/search":
+            logo = "SEARCH"
+            break;
+
+        case "/profile":
+            logo = "PROFILE"
+            break;
+    }
+
+    return(
+        <div className="logo" style={{color: pathname==="/profile"? "#ffffff":"#000000"}}>{logo}</div>
+    )
 }
 
 export default Logo;
---
+++ src/components/pages/Profile/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { MainContainer, MainScale } from '../../../utils';
+
+const ProfilePage = () => {
+    return(
+        <MainScale>
+            <MainContainer>
+                profile..
+            </MainContainer>
+        </MainScale>
+    )
+}
+
+export default ProfilePage;
